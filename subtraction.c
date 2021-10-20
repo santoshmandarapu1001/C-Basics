e41e0be388dedc29a483412ed7b92ae83d97cd83
santoshmandarapu1001 <92290829+santoshmandarapu1001@users.noreply.github.com>
2021-10-20 12:28:25 +0530

    Add files via upload

---
+++ subtraction.c
@@ -0,0 +1,11 @@
+// c program to print the subtraction og given numbers is
+#include<stdio.h>
+void main()
+{
+	int no1,no2,sum;
+	no1=65;
+	no2=25;
+	sum=no1-no2;
+	printf("subtraction of given numbers is %d",sum);
+	
+}
